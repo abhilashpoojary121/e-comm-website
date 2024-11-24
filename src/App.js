394b6d9986fa89988b00b85ce3af3569767947dc
Abhilash Poojary <abhilashpoojary007@gmail.com>
2024-11-24 18:56:04 +0530

    Header development.

--- src/App.js
+++ src/App.js
@@ -4,18 +4,10 @@ function App() {
   return (
     <>
       <div id="header-container">
-        <img src={monk}></img>
-        <h1>Monk Upsell & Cross-sell</h1>
+        <img src={monk} width="30.97px" height="30.97px"></img>
+        <h3>Monk Upsell & Cross-sell</h3>
+        <hr />
       </div>
-      <h2>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
-      </h2>
     </>
   );
 }
